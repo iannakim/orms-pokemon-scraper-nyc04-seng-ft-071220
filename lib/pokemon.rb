3f56854b494ab65cafc123aef2430bfc66f11429
ksolomon7 <kalkidan.solomon2@gmail.com>
2020-08-12 12:44:20 -0400

    first method

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+    
+    @@all=[]
+
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:,name:,type:,db:)
+        @id=id
+        @name=name
+        @type=type
+        @db=db
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.save(name_arg, type_arg, @db)
+        @@all << SELECT * FROM pokemon WHERE name = name_arg
+    end
 end
